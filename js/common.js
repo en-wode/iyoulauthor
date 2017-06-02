0626d2b27c01116cd082f353c7ab2578f2523989
en-wode <875957823@qq.com>
2017-06-02 16:48:26 +0800

    aside

--- js/common.js
+++ js/common.js
@@ -2,17 +2,11 @@
  * Created by dell on 2017/6/1.
  */
 $(function () {
+    $(".contain dl:first-child").find("dd").show(1000);
     $(".contain dl").each(function () {
-        var d = true;
         $(this).find("dt").click(function () {
             $(this).siblings('dd').slideToggle('fast');
-            if(d){
-                $(this).css({background:"#a1b7c9",color:"white"});
-                d = false;
-            }else {
-                $(this).css({background:"white",color:"black"});
-                d = true;
-            }
+            $(this).toggleClass("lf_active");
         })
     })
 })
