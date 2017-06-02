ca1edc6cc9fe9d19ed014a18ad2bbd4833435ee8
en-wode <875957823@qq.com>
2017-06-02 18:10:12 +0800

    aside

--- js/common.js
+++ js/common.js
@@ -2,7 +2,6 @@
  * Created by dell on 2017/6/1.
  */
 $(function () {
-    $(".contain dl:first-child").find("dd").show(1000);
     $(".contain dl").each(function () {
         $(this).find("dt").click(function () {
             $(this).siblings('dd').slideToggle('fast');
