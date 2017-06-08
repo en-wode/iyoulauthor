200f2ad66acc37332b51772ac63b925c980a45e2
en-wode <875957823@qq.com>
2017-06-08 15:49:54 +0800

    细节调整

--- js/common.js
+++ js/common.js
@@ -8,4 +8,19 @@ $(function () {
             $(this).toggleClass("lf_active");
         })
     })
+    $(".contain dl dd li").click(function (){
+        $(this).addClass("active").siblings().removeClass("active");
+        var zpindex = $(this).index();
+        $("section").css("display","none").eq(zpindex).css("display","inline-block");
+    })
+    // zpgl
+    $(".to_cgx").click(function () {
+        $('section').css('display','none').parent().find('.cgx').css('display','inline-block')
+    })
+    $(".to_tjzp").click(function () {
+        $('section').css('display','none').parent().find('.xgzj1').css('display','inline-block')
+    })
+    $(".settime").click(function () {
+        $(".kexuan").removeAttr("disabled").removeClass("kexuan");
+    })
 })
